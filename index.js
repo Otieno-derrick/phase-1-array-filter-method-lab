3b653bc89b3ce5214acc028845b3b1c0cd7ed738
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-06-17 13:20:05 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+function findMatching(names, sought) {
+  return names.filter(maTching => maTching.toLowerCase() === sought.toLowerCase());
+  
+}
+
+function fuzzyMatch(names, teStdriverNames) {
+  return names.filter(myArray => myArray.toLowerCase().indexOf(teStdriverNames.toLowerCase()) === 0);
+}
+
+function matchName(names, driverName) {
+  return names.filter(record => record.name === driverName);
+}
